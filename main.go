aec33a1dd2e6ac352532013fb26868321ea1c61f
Nurio Fernandez <personal@nurio.me>
2022-10-02 03:49:22 +0200

    Added testing implementation

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/xXNurioXx/go-gta-sa-driver/driver"
+	"github.com/xXNurioXx/go-gta-sa-driver/math"
+)
 
 func main() {
-	fmt.Println("Hi")
+	driver := driver.New()
+	gtaSa := driver.GtaSa
+
+	vehicleLocation := gtaSa.GetVehicle().GetLocation()
+	fmt.Printf("Vehicle location: '%f', '%f', '%f'\n", vehicleLocation.X, vehicleLocation.Y, vehicleLocation.Z)
+
+	// Testing ciruit
+	driver.DriveToPos(math.Location{
+		X: -1278,
+		Y: -292,
+	})
+	driver.DriveToPos(math.Location{
+		X: -1398,
+		Y: -154,
+	})
+	driver.DriveToPos(math.Location{
+		X: -1282,
+		Y: 9,
+	})
+
+	// Repeat
+	driver.DriveToPos(math.Location{
+		X: -1278,
+		Y: -292,
+	})
+	driver.DriveToPos(math.Location{
+		X: -1398,
+		Y: -154,
+	})
+	driver.DriveToPos(math.Location{
+		X: -1282,
+		Y: 9,
+	})
+
 }
